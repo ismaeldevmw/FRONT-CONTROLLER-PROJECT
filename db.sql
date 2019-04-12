94e80510c9dad22cf5be1a3665daaad67383bcbd
Ismael López <lc.ismael@hotmail.com>
2019-04-12 17:47:26 -0500

    added sql script"

---
+++ db.sql
@@ -0,0 +1,3 @@
+create table jobs(idjob int auto_increment primary key, title varchar(45), description varchar(150));
+
+ create table users(iduser int auto_increment primary key, email varchar(150), password varchar(150));
